c3476903b354a70914329b259d1f1284d1a0a996
altersaddle <malcolm@ipatch.ca>
2016-01-10 14:48:37 -0800

    Use stored procedures for search
    Removed duplicated code

--- web/search.php
+++ web/search.php
@@ -57,29 +57,6 @@ if (isset($_POST['action']))
 		foodie_AddFooter();
 		exit();
 	}
-	if (eregi("[!|#§+*<>^?£$%&\/\\]", $_POST['search_text']))
-	{
-		echo "<p class=\"error\">" . MSG_INPUT_FIELD ." <em>{$_POST['search_text']}</em> " . MSG_INPUT_DANGER .".<br>\n";	
-		echo "<form method=\"post\" action=\"search.php"; if ($trans_sid == 0) { echo "?" . SID; } echo "\">\n";
-		echo "<input type=\"hidden\" name=\"action\" value=\"search\">\n
-		<p>" . MSG_SEARCH_INSERT_STRING . ":<br>\n
-		(" . MSG_SEARCH_INSERT_PARTIAL . ")<br>\n
-		<input type=\"text\" name=\"search_text\" size=\"80\">\n
-		<p>" . MSG_SEARCH_INSERT_FIELD . ":<br>\n
-		<select name=\"search_field\">\n
-		<option value=\"name\">" . MSG_RECIPE_NAME . "</option>\n
-		<option value=\"mainingredient\">" . MSG_RECIPE_MAIN . "</option>\n
-		<option value=\"ingredients\">" . MSG_RECIPE_INGREDIENTS . "</option>\n
-		<option value=\"description\">" . MSG_RECIPE_DESCRIPTION . "</option>\n
-		<option value=\"notes\">" . MSG_RECIPE_NOTES . "</option>\n
-		<option value=\"wines\">" . MSG_RECIPE_WINES . "</option>\n
-		<option value=\"all\">" . MSG_SEARCH_ALLFIELDS . "</option>\n
-		</select>\n
-		<p><input type=\"submit\" value=\"" . BTN_SEARCH . "\">\n
-		</form>\n";
-		foodie_AddFooter();
-		exit();
-	}
 	if ($_POST['action'] == "search")
 	{
 		//Split search_text variable entering it into an array
@@ -88,134 +65,95 @@ if (isset($_POST['action']))
 		{
 			$search_terms = explode(" ", $_POST['search_text']);
 		}
-		//Execute this code only if more than one term was
-		//entered
-		//Revise this code since it gives a warning for
-		//undefined variable with single term searches
-		if (@is_array($search_terms))
-		{
-			if ($_POST['search_field'] == "all")
-			{
-				echo "<p>" . MSG_SEARCH_STRING . " <strong>{$_POST['search_text']}</strong> " . MSG_SEARCH_FOUND . ":<br>\n";
-				foreach ($search_terms as $single_term)
-				{
-					$sql_multi_search_all = "SELECT * FROM main WHERE name LIKE '$single_term' OR mainingredient LIKE '$single_term' OR ingredients LIKE '$single_term' OR description LIKE '$single_term' OR notes LIKE '$single_term' OR wines LIKE '$single_term' ORDER BY name DESC";
-					if (!$search_result = $dbconnect->query($sql_multi_search_all))
-					{
-						echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . $search_result->error();
-						foodie_AddFooter();
-						exit();
-					}
-					//count results
-					$num_hits = $search_result->num_rows;
-					//if result == 0
-					if ($num_hits == 0)
-					{
-						echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
-					}
-					else
-					{
-						echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
-					}
-					
-					while ($search_multi_all_item = $search_result->fetch_object())
-					{
-						echo "<p><a href=\"recipe.php?recipe={$search_multi_all_item->id}\">{$search_multi_all_item->name}</a>\n";
-					}
-				}
-				foodie_AddFooter();
-				exit();
-			}
-			else
-			{
-				foreach ($search_terms as $single_term)
-				{
-					//Search on single fields		
-					echo "<p>" . MSG_SEARCH_STRING . " <strong>$single_term</strong> " . MSG_SEARCH_FIELD . ":<br>\n";
-					//Insert here code
-					$sql_multi_search = "SELECT * FROM main WHERE {$_POST['search_field']} LIKE '%$single_term%' ORDER BY name DESC";
-					if (!$exec_multi_search = mysql_query($sql_multi_search))
-					{
-						echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . mysql_error();
-						foodie_AddFooter();
-						exit();
-					}
-					//count results
-					$num_hits = mysql_num_rows($exec_multi_search);
-					//if result == 0
-					if ($num_hits == 0)
-					{
-						echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
-					}
-					else
-					{
-						echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
-					}
+        else {
+            // an array of one
+            $search_terms = array($_POST['search_text']);
+        }
 
-					while ($search_multi_item = mysql_fetch_object($exec_multi_search))
-					{
-						echo "<p><a href=\"recipe.php?recipe=$search_multi_item->id"; if ($trans_sid == 0) { echo "&" . SID; } echo "\">$search_multi_item->name</a>\n";
-					}
+        // Get search term
+        $search_field = $_POST['search_field'];
+        $search_field_check = array ('name', 'mainingredient', 'ingredients', 'description', 'notes', 'wines', 'all'); 
+        if (!in_array($search_field, $search_field_check)) {	
+            echo "<p class=\"error\">" . ERROR_ILLEGAL_REQUEST ."!\n";
+	        foodie_AddFooter();
+	        exit();
+        }
+
+		if ($search_field == "all")
+		{
+			echo "<p>" . MSG_SEARCH_STRING . " <strong>{$_POST['search_text']}</strong> " . MSG_SEARCH_FOUND . ":<br>\n";
+			foreach ($search_terms as $single_term)
+			{
+				$stmt = "SELECT * FROM main WHERE name LIKE ? OR mainingredient LIKE ? OR ingredients LIKE ? OR description LIKE ? OR notes LIKE ? OR wines LIKE ? ORDER BY name DESC";
+                $parametercount = 0;
+                while ($parametercount < $stmt->param_count) {
+                    $stmt->bind_param('s', "%{$single_term}%");
+                    $parametercount++;
+                }
+                $stmt->execute();
+				if (!$search_result = $stmt->get_result())
+				{
+					echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . $search_result->error();
+					foodie_AddFooter();
+					exit();
 				}
-				foodie_AddFooter();
-				exit();
-			}
-		}
-		//Search text into all searchable fields
-		if ($_POST['search_field'] == "all")
-		{
-			$sql_search_all = "SELECT * FROM main WHERE name LIKE '%{$_POST['search_text']}%' OR mainingredient LIKE '%{$_POST['search_text']}%' OR ingredients LIKE '%{$_POST['search_text']}%' OR description LIKE '%{$_POST['search_text']}%' OR notes LIKE '%{$_POST['search_text']}%' OR wines LIKE '%{$_POST['search_text']}%' ORDER BY name DESC";
-			if (!$search_result = $dbconnect->query($sql_search_all))
-			{
-				echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . $search_result->error();
-				foodie_AddFooter();
-				exit();
-			}
-			echo "<p>" . MSG_SEARCH_STRING . " <strong>{$_POST['search_text']}</strong> " . MSG_SEARCH_FOUND . ":\n";
-			//count results
-			$num_hits = $search_result->num_rows;
-			//if result == 0
-			if ($num_hits == 0)
-			{
-				echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
-			}
-			else
-			{
-				echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
-			}
-			while ($search_all_item = $search_result->fetch_object())
-			{
-				echo "<p><a href=\"recipe.php?recipe=$search_all_item->id"; if ($trans_sid == 0) { echo "&" . SID; } echo "\">$search_all_item->name</a>\n";
+				//count results
+				$num_hits = $search_result->num_rows;
+				//if result == 0
+				if ($num_hits == 0)
+				{
+					echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
+				}
+				else
+				{
+					echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
+				}
+					
+				while ($search_multi_all_item = $search_result->fetch_object())
+				{
+					echo "<p><a href=\"recipe.php?recipe={$search_multi_all_item->id}\">{$search_multi_all_item->name}</a>\n";
+				}
 			}
 			foodie_AddFooter();
 			exit();
 		}
-		//Search on specified field
-		$sql_search = "SELECT * FROM main WHERE {$_POST['search_field']} LIKE '%{$_POST['search_text']}%' ORDER BY name DESC";
-		if (!$search_result = $dbconnect->query($sql_search))
-		{
-			echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . $search_result->error();
-			foodie_AddFooter();
-			exit();
-		}
-		echo "<p>" . MSG_SEARCH_STRING . " <strong>{$_POST['search_text']}</strong> " . MSG_SEARCH_FOUND . ":\n";
-		//count results
-		$num_hits = $search_result->num_rows;
-		//if result == 0
-		if ($num_hits == 0)
-		{
-			echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
-			}
 		else
 		{
-			echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
+			foreach ($search_terms as $single_term)
+			{
+				//Search on single fields		
+				echo "<p>" . MSG_SEARCH_STRING . " <strong>$single_term</strong> " . MSG_SEARCH_FIELD . ":<br>\n";
+				//Insert here code
+				$sql_multi_search = "SELECT * FROM main WHERE {$search_field} LIKE '%$single_term%' ORDER BY name DESC";
+                $stmt = "SELECT * FROM main WHERE {$search_field} LIKE ? ORDER BY name DESC";
+                $stmt->bind_param('s', "%{$single_term}%");
+                $stmt->execute();
+				if (!$search_result = $stmt->get_result())
+				{
+					echo "<p class=\"error\">" . ERROR_SEARCH_DATABASE . "<br>\n" . $search_result->error()();
+					foodie_AddFooter();
+					exit();
+				}
+				//count results
+				$num_hits = $search_result->num_rows;
+				//if result == 0
+				if ($num_hits == 0)
+				{
+					echo "<p class=\"error\">" . MSG_SEARCH_NORECIPESFOUND . "</p>\n";
+				}
+				else
+				{
+					echo "<p class=\"error\">" . MSG_SEARCH_RECIPESFOUND . ": $num_hits</p>\n";
+				}
+
+				while ($search_multi_item = $search_result->fetch_object())
+				{
+					echo "<p><a href=\"recipe.php?recipe=$search_multi_item->id\">$search_multi_item->name</a>\n";
+				}
+			}
+			foodie_AddFooter();
+			exit();
 		}
-		while ($search_item = $search_result->fetch_object())
-		{
-			echo "<p><a href=\"recipe.php?recipe=$search_item->id"; if ($trans_sid == 0) { echo "&" . SID; } echo "\">$search_item->name</a>\n";
-		}
-		foodie_AddFooter();
-		exit();
 	}
 	else
 	{
